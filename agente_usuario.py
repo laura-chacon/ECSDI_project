2ba95de53fc8e97b8cd3c56ada54dc6f31fedb17
Susan Pamela Henriquez Elias <susan.pamela.henriquez@c6s304mac05.fib.upc.es>
2016-05-12 11:47:30 +0200

    Load ontology

--- agente_usuario.py
+++ agente_usuario.py
@@ -1,11 +1,16 @@
 from flask import Flask
 import requests
+from rdflib import Graph
 
 app = Flask(__name__)
 
 @app.route('/busqueda')
 
 def busqueda():
+  g = Graph()
+  g.parse('/home2/users/alumnes/1149684/dades/linux/ECSDI/ProjectX.owl', format='xml')
+  for s, p, o in g:
+    print s, p, o
   peticion = {'categoria': 'electronica'}
   r = requests.get('http://127.0.0.1:9001/busqueda_productos', params=peticion)
   return r.text
